ff85c3d5257253ebf5d8c81f8fca85a738632e80
Renate <renate.gouveia@gmail.com>
2024-06-29 20:42:10 +0800

    Add example

--- apps/web/src/app/app/dashboard/page.tsx
+++ apps/web/src/app/app/dashboard/page.tsx
@@ -1,16 +1,18 @@
-"use client";
 import { Button } from "@/components/ui/button";
 import { userService } from "services";
-// import { signOut } from "@/lib/next-auth.lib";
-import { signOut } from "next-auth/react";
+import { signOut } from "@/lib/next-auth.lib";
 
 const DashboardPage = () => {
-  const handleSignOut = async () => {
-    await signOut();
-  };
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button onClick={handleSignOut}>DASHBOARD</Button>
+      <form
+        action={async () => {
+          "use server";
+          await signOut();
+        }}
+      >
+        <Button type="submit">Sign out</Button>
+      </form>
     </main>
   );
 };
