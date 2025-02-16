853fea69b87aa6abba23e22f0faa61b11d7b8eae
renate-hq <renate@hq.xyz>
2025-02-16 21:23:09 +0800

    Comment on search

--- apps/web/app/dashboard/(overview)/page.tsx
+++ apps/web/app/dashboard/(overview)/page.tsx
@@ -9,8 +9,8 @@ const DashboardPage = async ({
     page?: string;
   };
 }) => {
-  const query = searchParams?.query || "";
-  const currentPage = Number(searchParams?.page) || 1;
+  // const query = searchParams?.query || "";
+  // const currentPage = Number(searchParams?.page) || 1;
   // const posts = await postService.getPosts({ page: 1, limit: 10 });
 
   return (
